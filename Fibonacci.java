2793243dd54543453d4cc44fcb5d73b4c57575ec
BernardoFernandes <32074494+BernardoFernandes@users.noreply.github.com>
2018-04-19 15:02:27 +0100

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,22 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci { 
+    
+    public static void main(String[] args) {
+       
+          int num1 = 1, num2 = 0;
+        
+        System.out.println(num2);
+        System.out.println(num1);
+        
+        for(int i = 0; i < 17; i++){
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            System.out.println(num1);
+        }
+    }
+    
+}
+
